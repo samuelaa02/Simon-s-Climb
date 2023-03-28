3e2d5c6ab11923258ee7a935465bbb08b94f2660
Sam Austin <samuelaa02@gmail.com>
2023-03-28 13:41:27 -0400

    started implementing level file parsing

--- Stage.py
+++ Stage.py
@@ -1,6 +1,6 @@
 # object responsible for handling current stage layout information
 
-import pygame, sys
+import pygame, sys, array
 
 
 class Stage:
@@ -13,16 +13,47 @@ class Stage:
 
     def load_level(self):
         with open(self.filepath) as file:
+
+            self.stageName = file.readline()
+            stageDim = file.readline().split()
+            self.dimensions = pygame.math.Vector2(int(stageDim[0]), int(stageDim[1]))
+            self.background = file.readline()
+            spawnPt = file.readline().split
+            self.spawnPoint = pygame.math.Vector2(int(spawnPt[0]), int(spawnPt[1]))
+
+            if(file.readline() != "Platforms"):
+                sys.exit("~~Expected \"Platforms\" title after spawnpoint information.~~")
+            self.stageLayout[self.dimensions.x][self.dimensions.y]
+            tempPlatformInfo = file.readline()
+            while(tempPlatformInfo != "Enemies"):
+                tempPlatformInfo.split()
+                try:
+                    self.stageLayout[int(tempPlatformInfo[1])][int(tempPlatformInfo[2])] = Platform(tempPlatformInfo[0],int(tempPlatformInfo[1]),int(tempPlatformInfo[2]))
+                except:
+                    sys.exit("~~Invalid platform definition.~~\n")
+            
+
+
             for line in file:
                 #read in and validate information
-                
+                #read level name
+                #read width, height
+                #read bg file path
+                #spawn point
+                #platforms
+                #type posx posy
+                #enemies
+                #type posx posy
+                #collectibles
+                #type posx posy
+
                 pass
     
 
     #needs work
 class Platform:
-    def __init__(self, material, variant, posx, posy):
+    def __init__(self, material, posx, posy):
         self.material = material
-        self.variant = variant
+        self.variant = 0
         self.position = pygame.math.Vector2(posx, posy)
 
